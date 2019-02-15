dabb61a2cbd0c29becd487803bf6d78c26c37380
Lxqing <66@qq.com>
2019-02-15 19:34:56 +0800

    用户登录功能实现完毕

---
+++ src/components/Login.vue
@@ -0,0 +1,114 @@
+<template>
+  <div id="login-container">
+    <div id="login-box">
+      <div id="logo-box">
+        <img src="../assets/img/logo.png" alt>
+      </div>
+
+      <el-form ref="form" :rules="loginFormRules" :model="loginForm">
+        <el-form-item prop="username">
+          <el-input v-model="loginForm.username">
+            <i slot="prefix" class="iconfont icon-user"></i>
+          </el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input type="password" v-model="loginForm.password">
+            <i slot="prefix" class="iconfont icon-3702mima"></i>
+          </el-input>
+        </el-form-item>
+        <el-row>
+          <el-col :offset="15">
+            <el-button type="primary" @click="login">登录</el-button>
+            <el-button type="info" @click="resetForm">重置</el-button>
+          </el-col>
+        </el-row>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      loginForm: {
+        username: '',
+        password: ''
+      },
+      loginFormRules: {
+        username: [
+          // 非空    错误提示    触发校验规则
+          { required: true, message: '请输入用户名称', trigger: 'blur' }
+        ],
+        password: [{ required: true, message: '请输入密码', trigger: 'blur' }]
+      }
+    }
+  },
+  methods: {
+    //   用户登录系统
+    login() {
+      // 对登录的form表单进行整体的校验
+      this.$refs.form.validate(async valid => {
+        if (valid === true) {
+          // axios 带着用户信息去后端数据库校验
+          const { data: res } = await this.$http.post('./login', this.loginForm)
+          // console.log(res)
+          if (res.meta.status !== 200) {
+            return this.$message.error('用户名或密码不存在')
+          }
+          // 通过浏览器的sessionStorage记录服务器返回的taken信息
+          window.sessionStorage.setItem('token',res.data.token)
+          //   页面重定向到后台首页
+          // this.$router.push('/home')
+        }
+      })
+    },
+    resetForm() {
+      this.$refs.form.resetFields()
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+#login-container {
+  background-color: #2b4b6b;
+  height: 100%;
+  overflow: hidden;
+  #login-box {
+    width: 450px;
+    height: 304px;
+    background-color: #fff;
+    border-radius: 4px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    .el-form {
+      position: absolute;
+      bottom: 0px;
+      width: 100%;
+      padding: 20px;
+      box-sizing: border-box;
+    }
+    #logo-box {
+      width: 130px;
+      height: 130px;
+      border-radius: 50%;
+      border: 1px solid #eee;
+      padding: 8px;
+      box-shadow: 0 0 10px #eee;
+      position: absolute;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      background-color: #fff;
+      img {
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+        background-color: #eee;
+      }
+    }
+  }
+}
+</style>
